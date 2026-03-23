818fb7fb17030c058e2ec6df2a3e43f4ddf65b72
iridataenergo <iri@dataenergo>
2026-03-23 08:33:53 +0100

    prvni verze

---
+++ config.py
@@ -0,0 +1,15 @@
+﻿import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("ENTSOE_TOKEN")
+
+AREAS = {
+    "CZ": "10YCZ-CEPS-----N",
+    "DE": "10Y1001A1001A83F",
+    "AT": "10YAT-APG------L",
+    "SK": "10YSK-SEPS-----K",
+    "PL": "10YPL-AREA-----S",
+}
+
+DISPLAY_TZ = "Europe/Prague"
